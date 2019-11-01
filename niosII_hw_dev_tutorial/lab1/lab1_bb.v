a546e1cb5155b29b73dfec7e382ca59113cf3ca1
allexmagno <allex.m@aluno.ifsc.edu.br>
2019-11-01 17:16:00 -0300

    com erro mas vai

--- niosII_hw_dev_tutorial/lab1/lab1_bb.v
+++ niosII_hw_dev_tutorial/lab1/lab1_bb.v
@@ -1,8 +1,10 @@
 
 module lab1 (
 	clk_clk,
-	reset_reset_n);	
+	reset_reset_n,
+	led_pio_external_connection_export);	
 
 	input		clk_clk;
 	input		reset_reset_n;
+	output	[7:0]	led_pio_external_connection_export;
 endmodule
